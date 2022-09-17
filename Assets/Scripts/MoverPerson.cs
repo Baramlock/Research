3b38cd84457082101a3575745ca6f133b042b172
Ямковой Данил <danil1998.01.08@outlook.com>
2022-09-17 13:09:45 +0500

    Movment

---
+++ Assets/Scripts/MoverPerson.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class MoverPerson
+    {
+        private Transform _transform;
+        private MovePersonSetting _currentSetting;
+
+        public MoverPerson(Transform transform, MovePersonSetting movePersonSetting)
+        {
+            _transform = transform;
+            _currentSetting = movePersonSetting;
+        }
+
+        public Vector3 Move(Vector2 direction, RaycastHit hit)
+        {
+            var selfDirection = ChooseMoveDirection(direction);
+            var directionParallelPlace = selfDirection - Vector3.Project(selfDirection, hit.normal);
+            var directionSpeed = directionParallelPlace * _currentSetting.FullSpeed;
+            _transform.position += directionSpeed;
+            return directionParallelPlace;
+        }
+
+        private Vector3 ChooseMoveDirection(Vector2 direction) =>
+            (_transform.forward * (direction.x * _currentSetting.ForwardSpeed) +
+             _transform.right * (direction.y * _currentSetting.LateralSpeed)) / 2;
+    }
+
+    [System.Serializable]
+    public class MovePersonSetting
+    {
+        [SerializeField] private float _fullSpeed = 1;
+        [SerializeField] private float _forwardSpeed = 1;
+        [SerializeField] private float _lateralSpeed = 1;
+
+        public float ForwardSpeed => _forwardSpeed;
+        public float LateralSpeed => _lateralSpeed;
+
+        public float FullSpeed => _fullSpeed;
+    }
+}
---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class Person : MonoBehaviour
+    {
+        [SerializeField] private MovePersonSetting _movePersonSetting;
+        [SerializeField] private RotatorPerson.RotatorPersonSetting _rotatorPersonSetting;
+        private InputKeyReader _inputKeyReader;
+        private MoverPerson _moverPerson;
+        private RotatorPerson _rotatorPerson;
+
+        private Vector3 _directionPlaceParallel;
+
+        private void Awake()
+        {
+            InitMove();
+        }
+
+        private void Update()
+        {
+            Move();
+        }
+
+        private void InitMove()
+        {
+            _inputKeyReader = new InputKeyReader();
+            _moverPerson = new MoverPerson(transform, _movePersonSetting);
+            _rotatorPerson = new RotatorPerson(transform, _rotatorPersonSetting);
+        }
+
+        private void Move()
+        {
+            var direction = _inputKeyReader.ReadDirection() * Time.deltaTime;
+
+            if (direction == Vector2.zero)
+                return;
+            Physics.Raycast(new Ray(transform.position, Vector3.down), out var hit);
+            _directionPlaceParallel = _moverPerson.Move(direction, hit);
+            _rotatorPerson.SlerpRotate(Vector3.forward, _directionPlaceParallel, Time.deltaTime);
+        }
+    }
+}
